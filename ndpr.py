54d84ca6739a0c8689cb80002422648f483ee3ad
Paulus Esterhazy <pesterhazy@gmail.com>
2010-07-24 23:14:49 +0200

    Major changes: drop html2ps, and use pandoc and latex (xetex) instead.

---
+++ ndpr.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import sys, os, re
+from BeautifulSoup import BeautifulSoup
+import urllib2
+import subprocess, tempfile, shutil
+from optparse import OptionParser
+
+class LRBParser:
+    format = { "title":"LRB" }
+
+    def extract(self,txt):
+        s = BeautifulSoup(txt)
+        for x in s.findAll(style="display:none"):
+            x.extract()
+
+        i=s.find('div',"article-body indent")
+        t=s.find('title')
+        i=("<html>" + str(t) + "<body>" + str(i) + "</body></html>")
+        return str(i)
+
+class NDPRParser:
+    format = { "title":"NDPR" }
+
+    def extract(self,txt):
+        txt = re.sub("</?o:p>","",txt)
+        txt = re.sub("&(?!\w*;)","&amp;",txt)
+
+        soup = BeautifulSoup(txt)
+        soup = soup.find('div',id="review")
+        for tag in soup.findAll("a"):
+            tag.replaceWith(tag.renderContents())
+
+        return str(soup)
+
+def runLatex(html):
+    tmpdir = tempfile.mkdtemp(prefix="offprint")
+    try:
+        i = tmpdir + "/i.html"
+        o = tmpdir + "/o.tex"
+        p = tmpdir + "/o.pdf"
+
+        f = open(i, "wb")
+        f.write(html)
+        f.close()
+
+        subprocess.check_call(["pandoc", "-f", "html", "-st", "latex", "--xetex",
+            "-o", o, i])
+        subprocess.check_call(["xelatex", "-interaction=batchmode", "-output-directory", tmpdir, o])
+
+        pdf = open(p, "rb").read()
+        if len(pdf) < 1000:
+            raise Exception("Invalid PDF")
+    finally:
+#        print(p)
+#        sys.stdin.readline()
+        shutil.rmtree(tmpdir)
+    
+    return pdf
+
+def convert(url):
+    html = urllib2.urlopen(url).read()
+    typeparser = NDPRParser()
+    html = typeparser.extract(html)
+    pdf = runLatex(html)
+    return pdf
+
+def main():
+    parser = OptionParser("usage: %prog [options] URL/file out.pdf")
+
+    parser.add_option("-n","--ndpr",action="store_const",dest="type",const="ndpr",
+        help="Notre Dame Philosophical Review")
+    parser.add_option("-l","--lrb",action="store_const",dest="type",const="lrb",
+        help="London Review of Books")
+    parser.set_defaults(type="ndpr")
+
+    (options,args) = parser.parse_args()
+    if len(args) < 1:
+        parser.error("incorrect number of arguments")
+
+    a = args[0]
+    try:
+        of = args[1]
+    except IndexError:
+        if a.lower().startswith("http://"):
+            of = "out.pdf"
+        else:
+            root = os.path.splitext(os.path.basename(a))[0]
+            of = root + ".pdf"
+
+    if options.type == "lrb":
+        typeparser = LRBParser()
+    else:
+        typeparser = NDPRParser()
+
+    if a.lower().startswith("http://"):
+        print("Fetching URL: " + a)
+        f = urllib2.urlopen(a).read()
+    else:
+        f = open(a).read()
+
+    print("Read %d bytes." % len(f))
+
+    # extract information
+    s=typeparser.extract(f)
+
+    pdf = runLatex(s)
+
+    print("Writing %s..." % of)
+    f=open(of,"w")
+    f.write(pdf)
+    f.close()
+
+if __name__ == "__main__":
+    main()
---
+++ offprintweb.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import re, yaml, os
+from flask import Flask, request, redirect, render_template, Response
+from ndpr import convert
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def about():
+    return render_template("form.html")
+
+@app.route('/go', methods=["POST"])
+def go():
+    url = request.form["url"]
+
+    m=re.search("review.cfm\?id=(\d*)",url)
+    if m:
+        filename = "ndpr-%s.pdf" % m.group(1)
+    else:
+        filename = "ndpr.pdf"
+
+    pdf = convert(url)
+
+    r = Response(pdf)
+    r.headers["content-type"] = "application/pdf"
+    r.headers["content-disposition"] = "attachment;filename=" + filename
+    r.headers["content-length"] = len(pdf)
+    return r
+#    return render_template("hurray.html",url=x)
+
+@app.route('/search')
+def search():
+    default = "g"
+
+    engines = Engines()
+
+    q = request.args["q"]
+
+    m = re.match(r'(\w+)\s+(.+)',q)
+
+    if m:
+        key, to = m.groups()
+        try:
+            url = engines.get(key).url
+        except KeyError:
+            to = q
+            url = engines.get_default().url
+    else:
+        url = engines.get_default().url
+        to = q
+
+    url = url % (to,)
+    return redirect(url)
+
+if __name__ == '__main__':
+    app.run()
